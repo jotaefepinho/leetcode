807bf1d85754d3225af0026a099a96ec885dbbf5
jotaefepinho <jota@Joaos-MacBook-Pro-2.local>
2024-09-27 19:26:21 -0300

    fix Jewels and Stones to use sets

--- numJewelsInStones.py
+++ numJewelsInStones.py
@@ -1,6 +1,10 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        set_jewels = set(jewels)
         total = 0
-        for i in jewels:
-            total += stones.count(i)
+
+        for stone in stones:
+            if stone in set_jewels:
+                total += 1
+        
         return total
