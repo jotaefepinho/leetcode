54b283bb9a042a035a665a41d261961fe2099d63
jotaefepinho <jotaefepinho@gmail.com>
2024-11-04 19:06:43 -0300

    add baseball game solution

---
+++ calPoints.py
@@ -0,0 +1,14 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        res = []
+        for i in operations:
+            if i == 'C':
+                res.pop()
+            elif i == 'D':
+                res.append(2 * res[-1])
+            elif i == '+':
+                res.append(res[-1] + res[-2])
+            else:
+                res.append(int(i))
+        print(res)
+        return sum(res)
