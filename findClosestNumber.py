b3a99c16f76b54ce31d10a2bc676efbf3c510fff
jotaefepinho <jotaefepinho@gmail.com>
2025-03-17 18:54:16 -0300

    update calPoints and findClosestNumber

--- findClosestNumber.py
+++ findClosestNumber.py
@@ -1,11 +1,4 @@
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        ans = nums[0]
-
-        for i in nums[1:]:
-            if abs(i) == abs(ans) and i > ans:
-                ans = i
-            if abs(i) < abs(ans):
-                ans = i
-        
-        return ans
+        nums.sort(key=lambda x: (abs(x), -x))
+        return nums[0]
