cc814e92b74340dfe6fc76e735c509a3462536aa
jotaefepinho <jotaefepinho@gmail.com>
2025-03-20 20:25:18 -0300

    add fib and climbStairs Solutions

---
+++ fib.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = (n + 1) * [-1]
+        
+        memo[0] = 0
+        memo[1] = 1
+
+        for i in range(2, n + 1):
+            print(i)
+            memo[i] = memo[i - 1] + memo[i - 2]
+        
+        return memo[n]
