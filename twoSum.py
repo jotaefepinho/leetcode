941567fbbe06e60ad81a40d709a584accb77916d
jotaefepinho <jotaefepinho@gmail.com>
2025-04-02 12:47:00 -0300

    add hashmap twoSum solution

--- twoSum.py
+++ twoSum.py
@@ -1,3 +1,16 @@
+# Solution with hashmap
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        remainder = {}
+
+        for i in range(len(nums)):
+            rem = target - nums[i]
+            if rem in remainder:
+                return remainder[rem], i
+            else:
+                remainder[nums[i]] = i
+
+# Solution with two pointers: Array must be sorted
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left = 0
